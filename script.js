695383797224051e02dd1fcb6f7bf5010ef4a8d7
Librus1311 <straxolud2015@gmail.com>
2025-10-16 06:54:12 +0300

    a che ya delal to

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// BURGER MENU
+
 const menuIcon = document.querySelector('.burger-menu-btn');
 const menu = document.querySelector('.burger-menu');
 
@@ -5,3 +7,38 @@ menuIcon.addEventListener('click', () => {
     menuIcon.classList.toggle('active');
     menu.classList.toggle('active');
 });
+
+// REVIEW SLIDER 
+
+const slider = document.querySelector('.main-reviews-block-list')
+const btnPrev = document.querySelector('.btn-prev')
+const btnNext = document.querySelector('.btn-next')
+
+const slides = Array.from(slider.querySelectorAll('.main-reviews-block-list-item'))
+const slideCount = slides.length
+let slideIndex = 0
+
+btnPrev.addEventListener('click', showPreviousSlide)
+btnNext.addEventListener('click', showNextSlide)
+
+function showPreviousSlide() {
+    slideIndex = (slideIndex - 1 + slideCount) % slideCount
+    updateSlider()
+}
+
+function showNextSlide() {
+    slideIndex = (slideIndex + 1) % slideCount
+    updateSlider()
+}
+
+function updateSlider() {
+    slides.forEach((slide, index) => {
+        if (index === slideIndex) {
+            slide.style.display = 'block'
+        } else {
+            slide.style.display = 'none'
+        }
+    })
+}
+
+updateSlider()
